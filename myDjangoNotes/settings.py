ce660d9a0062e367c01f4e7b00bdf56ae9c35613
edgarbahena1998@gmail.com <edgarbahena1998@gmail.com>
2024-04-23 09:50:36 -0700

    Updated urls

--- myDjangoNotes/settings.py
+++ myDjangoNotes/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # APPS CREATED BY USER
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
